43d9c2e7682f07fec8ab752b64c5bbca3766aaf5
Moises Sacal <moisbo@gmail.com>
2016-11-02 00:06:57 -0600

    added automatic dates

--- app/live.js
+++ app/live.js
@@ -4,13 +4,16 @@ const fs = require('fs');
 const moment = require('moment');
 const EventEmitter = require('events');
 
-class MyEmitter extends EventEmitter {}
+class MyEmitter extends EventEmitter {
+}
 const routes = require('./routes');
+const dates = require('./dates');
 const session = require('./session');
 const polling = require('./polling');
 const settings = require('./settings');
 
 let routeIndex = 0;
+let dateIndex = 0;
 const livePoll = new MyEmitter();
 
 const dir = settings.pollingDir;
@@ -21,7 +24,7 @@ const writeFile = (location, scan) => {
     if (iti) {
       iti = JSON.parse(iti);
       console.log('Status: ' + iti.Status);
-      const name = '_'+scan.originPlace+'_'+scan.destinationPlace;
+      const name = '_' + scan.originPlace + '_' + scan.destinationPlace;
       const file = dir + '/itinerary' + name + format + '.json';
       console.log(file);
       fs.writeFile(file, JSON.stringify(iti), (err) => {
@@ -35,16 +38,18 @@ const writeFile = (location, scan) => {
       writeFile(location, scan);
     } else {
       console.log('polling complete');
+      dateIndex++;
+      if (dateIndex >= dates.length) {
+        routeIndex++;
+        dateIndex = 0;
+      }
       livePoll.emit('event');
     }
   });
 };
 
-
 livePoll.on('event', () => {
-  if(routeIndex < routes.length){
-
-    console.log(routeIndex + ': ' + routes[routeIndex].in + ': ' + routes[routeIndex].out);
+  if (routeIndex < routes.length) {
     const scan = {
       country: 'AU',
       currency: 'AUD',
@@ -53,13 +58,12 @@ livePoll.on('event', () => {
       groupPricing: 'true',
       originPlace: routes[routeIndex].in,
       destinationPlace: routes[routeIndex].out,
-      outboundDate: '2016-11-04',
-      inboundDate: '2016-11-11',
+      outboundDate: dates[dateIndex].out,//'2016-11-04',
+      inboundDate: dates[dateIndex].in,//2016-11-11',
       adults: '1'
     };
-
-    routeIndex++;
-
+    console.log(routeIndex + ': ' + routes[routeIndex].in + ': ' + routes[routeIndex].out);
+    console.log('outboundDate: ' + scan.outboundDate + ', inboundDate: ' + scan.inboundDate);
     session(scan, (location, scan) => {
       writeFile(location, scan);
     });
